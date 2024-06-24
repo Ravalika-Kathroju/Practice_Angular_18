f38e2a5a24674efbd449eba5e7dff23be4d63c22
Ravalika KAthroju <ravalikakathroju.work@gmail.com>
2024-06-24 15:54:41 +0530

    Created new components and added assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { EmployeeListComponent } from './components/employee-list/employee-list.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { HOMEComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { AboutComponent } from './components/about/about.component';
@@ -11,7 +8,7 @@ import { ContactComponent } from './components/contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HOMEComponent, MenuComponent, AboutComponent, ContactComponent, AddEmployeeComponent,EmployeeListComponent,DataBindingComponent],
+  imports: [RouterOutlet, RouterLink, HOMEComponent, MenuComponent, AboutComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { Routes } from '@angular/router';
 import { HOMEComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { AboutComponent } from './components/about/about.component';
-import {ContactComponent} from './components/contact/contact.component';
-import { IfelseComponent } from './components/ifelse/ifelse.component';
-import {ForComponent} from './components/for/for.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 export const routes: Routes = [ //route array
     {//route object
@@ -23,13 +22,5 @@ export const routes: Routes = [ //route array
     {
         path:'Contact-route',
         component: ContactComponent
-    },
-    {
-        path:'ifelse-route',
-        component: IfelseComponent
-    },
-    {
-        path: 'for-route',
-        component: ForComponent
     }
 ];
